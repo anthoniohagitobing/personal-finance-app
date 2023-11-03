da4898725ea8b51724bdb6b1f1be30e6351111f7
Anthonio Hagi Tobing <133083562+anthoniohagitobing@users.noreply.github.com>
2023-11-03 19:09:32 +0900

    feat: setting up database 2

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import { PrismaClient } from '@prisma/client'
+
 
 // IMPORTING DATABASE CONTROLLER
 
@@ -13,50 +13,6 @@ dotenv.config({path: "./.env"});
 // USING MIDDLEWARE
 app.use(express.json());
 app.use(cors());
-
-
-const prisma = new PrismaClient()
-
-async function main() {
-  // ... you will write your Prisma Client queries here
-  await prisma.user.create({
-    data: {
-      name: 'Alice',
-      email: 'alice@prisma.io',
-      posts: {
-        create: { title: 'Hello World' },
-      },
-      profile: {
-        create: { bio: 'I like turtles' },
-      },
-    },
-  })
-
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
-  })
-  console.dir(allUsers, { depth: null })
-
-  const post = await prisma.post.update({
-    where: { id: 1 },
-    data: { published: true },
-  })
-  console.log(post)
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
-
 
 
 // ENDPOINTS
